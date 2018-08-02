d1a9936bbc8f2c4d4a5e0eab0a1c498b7b5c0d6a
黄文彬 <huang.wb@sunrizetech.com>
2018-08-02 15:40:36 +0800

    适配文件名

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
     <div id="app">
         <div>
             <el-row :gutter="20">
-                <el-col :span="5">
+                <el-col :span="8">
                     <file-tree></file-tree>
                 </el-col>
-                <el-col :span="18">
+                <el-col :span="16">
                     <router-view></router-view>
                 </el-col>
             </el-row>
--- src/component/Files.vue
+++ src/component/Files.vue
@@ -1,74 +1,50 @@
 <!--文件树-->
 <template>
-    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree :data="data" :props="defaultProps" :load="loadNode"
+             lazy>
+    </el-tree>
 </template>
 
 <script>
-export default {
-    methods: {
-        handleNodeClick(data) {
-            console.log(data);
-        }
-    },
-    data() {
-        return {
-            data: [{
-                label: '一级 1',
-                children: [{
-                    label: '二级 1-1',
-                    children: [{
-                        label: '三级 1-1-1'
-                    }]
-                }]
-            }, {
-                label: '一级 2',
-                children: [{
-                    label: '二级 2-1',
-                    children: [{
-                        label: '三级 2-1-1'
-                    }]
-                }, {
-                    label: '二级 2-2',
-                    children: [{
-                        label: '三级 2-2-1'
-                    }]
-                }]
-            }, {
-                label: '一级 3',
-                children: [{
-                    label: '二级 3-1',
-                    children: [{
-                        label: '三级 3-1-1'
-                    }]
-                }, {
-                    label: '二级 3-2',
-                    children: [{
-                        label: '三级 3-2-1'
-                    }]
-                }]
-            }],
-            defaultProps: {
-                children: 'children',
-                label: 'label'
-            },
-            tableData: [{
-                date: '2016-05-02',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
-            }, {
-                date: '2016-05-04',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1517 弄'
-            }, {
-                date: '2016-05-01',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1519 弄'
-            }, {
-                date: '2016-05-03',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1516 弄'
-            }]
+    export default {
+        methods: {
+            loadNode(node, resolve) {
+                resolve([
+                    {
+                        "name": "com/karl/debugger/ui",
+                        "size": 288,
+                        "lastModified": 1533024572000,
+                        "path": "com",
+                        "isDir": true
+                    },
+                    {
+                        "name": "application.properties",
+                        "size": 78,
+                        "lastModified": 1533024571000,
+                        "path": "application.properties",
+                        "isDir": false
+                    }
+                ]);
+
+            }
+        },
+        data() {
+            return {
+                data: [],
+                defaultProps: {
+                    label: function (data, node) {
+                        //将class后缀的文件转回java文件
+                        if (data.name.indexOf(".class") > 0) {
+                            return data.name.substr(0, data.name.indexOf(".class")) + ".java";
+                        } else {
+                            return data.name;
+                        }
+                    },
+                    isLeaf: function (data, node) {
+                        return !data.isDir;
+                    }
+                }
+            }
         }
     }
-}
 </script>
