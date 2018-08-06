54e0abaa92b5cde873bf46e23ec99cb8fbe40223
kawhii <huang.wenbin@foxmail.com>
2018-08-06 14:02:01 +0800

    新增从前端分支构建后copy到主干分支，主要在本地能够访问接口

---
+++ src/main/resources/public/debug-ui/vendor.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{132:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var c=t(1),o=(t.n(c),t(29));t.n(o)}},[132]);
+//# sourceMappingURL=vendor.js.map?c16be88fb265564d231b
